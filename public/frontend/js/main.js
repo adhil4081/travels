56e0d0d08b5edc5be1b4aebdd0e912f67f82e46e
adhil <adhilsakheer@gmail.com>
2023-11-14 17:21:44 +0400

    about section

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     var nav = document.querySelector(".nav");
-
+    var logo = document.querySelector(".nav .container .logo a");
+    var links = document.querySelectorAll(".nav div.main_list ul li a");
     window.addEventListener("scroll", function () {
-        var logo = document.querySelector(".nav .container .logo a");
-
         if (window.scrollY > 50) {
-            nav.style.backgroundColor = "black";
+            nav.style.backgroundColor = "#010b13";
             logo.style.color = "white";
+            links.forEach(function (link) {
+                link.style.color = "white";
+            });
         } else {
             nav.style = nav.style;
+            logo.style = logo.style;
+            links.forEach(function (link) {
+                link.style = link.style;
+            });
         }
     });
 });
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,7 +11,7 @@
                         <h1>Wonderfull world</h1>
                     </div>
                     <div class="col-md-12">
-                        <p>It's the perfect time travel and enjoy the 
+                        <p>It's the perfect time travel and enjoy the
                             beauty of the world.
                         </p>
                     </div>
@@ -21,12 +21,18 @@
         <section class="about">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6 left about">
-                        <div class="card-bg">
-                            
+                    <div class="col-md-6">
+                        <div class="card-bg-left">
+                            <img src="{{ asset('frontend/img/about.jpg') }}" alt="">
                         </div>
-
                     </div>
+
+                    <div class="col-md-6">
+                        <div class="card-bg-right">
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
